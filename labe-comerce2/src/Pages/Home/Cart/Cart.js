a27f4c82328d564dfa41b8c14ed92ad1ebe12d30
Fernanda <fernanda_tiyomi@hotmail.com>
2022-08-03 10:38:51 -0300

    criando projeto

---
+++ labe-comerce2/src/Pages/Home/Cart/Cart.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Cart() {
+  return (
+    <div>
+      <h1>Cart</h1>
+    </div>
+  );
+}
+export default Cart;
---
+++ labe-comerce2/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import * as C from "./Styled";
+
+function Header() {
+  return (
+    <C.AreaHeader>
+      <header>Header</header>
+    </C.AreaHeader>
+  );
+}
+export default Header;
