029b60f926333c48412ae64f8b520053e8bffffa
CarlosSousa2001 <carlosramos10k@gmail.com>
2022-08-03 15:25:15 -0300

    erros e erros

--- labe-comerce2/src/Pages/Home/Home.js
+++ labe-comerce2/src/Pages/Home/Home.js
@@ -1,27 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import * as C from "./Styled";
-import {Card} from "../../components/Card/Card";
+import { Card } from "../../components/Card/Card";
 
 function Home() {
-
+  const [order, setOrder] = useState("Crescente")
   const [produtos, setProdutos] = useState([
     {
       id: 1,
       imagem: "http://picsum.photos/30/30",
       name: "Selenita",
-      valor: "15.236,00"
+      valor: "100"
     },
     {
       id: 2,
       imagem: "http://picsum.photos/30/30",
       name: "Pirita",
-      valor: "15.200,00"
+      valor: "1500"
     },
     {
       id: 3,
       imagem: "http://picsum.photos/30/30",
       name: "Rubi",
-      valor: "15.800,00"
+      valor: "4000"
     },
     {
       id: 4,
@@ -31,15 +31,26 @@ function Home() {
     }
 
   ])
+  const onChangeOrder = (event) => {
+    setOrder(event.target.value)
+  }
+
 
   const listaDeProdutos = produtos.map((produto) => {
-    return <Card imagem={produto.imagem} name={produto.name} valor={produto.valor} produtos={produto}/>
+    return <Card imagem={produto.imagem} name={produto.name} valor={produto.valor} produtos={produto} />
   })
 
-
+  // const renderiza = produtos.map((produto) => {
+  //   return <Card imagem={produto.imagem} name={produto.name} valor={produto.valor} produtos={produto}/>
+  // })
   return (
-
     <C.Container>
+      <div>
+        <select value={order} onChange={onChangeOrder}>
+          <option value="Crescente">Crescente</option>
+          <option value="Decrescente">Decrescente</option>
+        </select>
+      </div>
       <C.ContainerFilho>
         {listaDeProdutos}
       </C.ContainerFilho>
--- labe-comerce2/src/components/Card/Card.js
+++ labe-comerce2/src/components/Card/Card.js
@@ -1,19 +1,17 @@
-import React from "react";
 import * as C from "./styled";
+import React, { useState, useEffect } from "react"
 
-export function Card(props) {
-
-
-
+export function Card(props) {  
     return (
-
-        <C.AreaCard>
-            <C.AreaCardFilho>
-                <h1>{props.name}</h1>
-                <img src={props.imagem}></img>
-                <p>{props.valor}</p>
-            </C.AreaCardFilho>
-        </C.AreaCard>
+        <div>
+            <C.AreaCard>
+                <C.AreaCardFilho>
+                    <h1>{props.name}</h1>
+                    <img src={props.imagem}></img>
+                    <p>{props.valor}</p>
+                </C.AreaCardFilho>
+            </C.AreaCard>
+        </div>
 
     )
 }
